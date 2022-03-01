819033fbf5332ad50dfbaf4d964c9dfcbf8ebd59
sorban.ivan <sorban.ivan@D20-06>
2022-03-01 11:35:24 +0100

    Add project files.

---
+++ OOP_auto/vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_auto
+{
+    public class vehicle
+    {
+        public string Type { get; set; }
+        public string Brand { get; }
+        public string Fuel { get; set; }
+        public double EngineVolume { get; set; }
+        public int FuelLevel { get; set; }
+        public int Distance { get; set; }
+        public int PneuState { get; set; }
+
+        public vehicle(string brand)
+        {
+            Type = "Unknown";
+            Brand = Brand;
+            Fuel = "Gas";
+            EngineVolume = 1000;
+            FuelLevel = 100;
+            Distance = 0;
+            PneuState = 100;
+        }
+
+    }
+}
